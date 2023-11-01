05de287957deff2a51335cb1d9c165dd0457467a
Sakshi Priya <priyasakshi8989@gmail.com>
2023-11-01 15:49:59 +0530

    Added Navbar

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,7 +1,9 @@
+import Navbar from "../components/Navbar";
 
 function Home (){
     return(
         <>
+        <Navbar />
         Home
         </>
     )
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,41 @@
+import DummyProfileName from '../Assets/DummyProfileName.jpg';
+import Logo from '../Assets/logo.jpg'
+
+function Navbar() {
+
+    return (
+
+        <div className="navbar bg-[#333545]">
+            <div className="flex-1">
+                <div className = 'h-12 mr-8'>
+                    <img className ="max-h-[100%] w-36"src={Logo} />
+                </div>
+                <div className="form-control w-3/5">
+                    <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
+                </div>
+            </div>
+            <div className="flex-none gap-2">
+                
+                <div className="dropdown dropdown-end">
+                    <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10 rounded-full">
+                            <img src={DummyProfileName} />
+                        </div>
+                    </label>
+                    <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm text-white dropdown-content bg-[#333545] rounded-box w-52">
+                        <li>
+                            <a className="justify-between">
+                                Profile
+                                <span className="badge">New</span>
+                            </a>
+                        </li>
+                        <li><a>Settings</a></li>
+                        <li><a>Logout</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
