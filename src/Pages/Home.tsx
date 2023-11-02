a47168b5457768256ab2029fec5ae61b2f4b79df
Sakshi Priya <priyasakshi8989@gmail.com>
2023-11-02 17:08:16 +0530

    Added carousel Images and implement

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,11 @@
+import HomeCarousel from "../components/HomeCarousel";
 import Navbar from "../components/Navbar";
 
 function Home (){
     return(
         <>
         <Navbar />
+        <HomeCarousel />
         Home
         </>
     )
---
+++ src/components/HomeCarousel.tsx
@@ -0,0 +1,41 @@
+import CarouselImages1 from '../Assets/c1.jpg';
+import CarouselImages2 from '../Assets/c2.jpg';
+import CarouselImages3 from '../Assets/c3.jpg';
+import CarouselImages4 from '../Assets/c4.jpg';
+
+function HomeCarousel() {
+    return (
+        <div className="carousel w-full">
+            <div id="slide1" className="carousel-item relative w-full">
+                <img src={CarouselImages1} className="w-full" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide4" className="btn btn-circle">❮</a>
+                    <a href="#slide2" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full">
+                <img src={CarouselImages2} className="w-full" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide1" className="btn btn-circle">❮</a>
+                    <a href="#slide3" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+                <img src={CarouselImages3} className="w-full" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide2" className="btn btn-circle">❮</a>
+                    <a href="#slide4" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+            <div id="slide4" className="carousel-item relative w-full">
+                <img src={CarouselImages4} className="w-full" />
+                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                    <a href="#slide3" className="btn btn-circle">❮</a>
+                    <a href="#slide1" className="btn btn-circle">❯</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomeCarousel;
