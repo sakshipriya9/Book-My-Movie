df0c6387a440f24ae2a49dff4e139c885dd02548
Sakshi Priya <priyasakshi8989@gmail.com>
2023-11-03 13:18:54 +0530

    Added HOme page card posters

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,21 @@
 import HomeCarousel from "../components/HomeCarousel";
+import HomeMovieCard from "../components/HomeMovieCard";
 import Navbar from "../components/Navbar";
+import MoviePoster from "../Assets/m1.jpg"
 
 function Home (){
     return(
         <>
         <Navbar />
         <HomeCarousel />
+
+        <div className="mt-8 flex justify-center items-center gap-10">
+            <HomeMovieCard movieImage={MoviePoster} />
+            <HomeMovieCard movieImage={MoviePoster} />
+            <HomeMovieCard movieImage={MoviePoster} />
+            <HomeMovieCard movieImage={MoviePoster} />
+
+        </div>
         Home
         </>
     )
